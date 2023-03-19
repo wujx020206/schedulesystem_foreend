12bcdd267629b54c0fd77f60867fd2eaa0f2d85a
吴佳熙 <1006961384@qq.com>
2023-03-19 23:40:44 +0800

    员工管理界面增加按钮和展示列表

--- src/views/StaffManagement .vue
+++ src/views/StaffManagement .vue
@@ -1,8 +1,234 @@
 <template>
+  <div class="card">
+    <button type="button" class="btn btn-success" style="height: 38px;width: 150px">新增</button>
+    <button type="button" class="btn btn-danger" style="height: 38px;width: 150px">删除</button>
+    <button type="button" class="btn btn-warning" style="height: 38px;width: 150px">修改</button>
+    <button type="button" class="btn btn-info" style="height: 38px;width: 150px">查询</button>
+    
+    <div class="table-responsive">
+      <table class="table align-items-center mb-0">
+        <thead>
+          <tr>
+            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">员工</th>
+            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">职位</th>
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">联系电话</th>
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">所属门店</th>
+            <th class="text-secondary opacity-7"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div class="d-flex px-2 py-1">
+                <div>
+                  <img src="https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/team-2.jpg" class="avatar avatar-sm me-3">
+                </div>
+              <div class="d-flex flex-column justify-content-center">
+                <h6 class="mb-0 text-xs">John Michael</h6>
+                <p class="text-xs text-secondary mb-0">john@creative-tim.com</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p class="text-xs font-weight-bold mb-0">Manager</p>
+            <p class="text-xs text-secondary mb-0">Organization</p>
+          </td>
+          <td class="align-middle text-center text-sm">
+            <span class="badge badge-sm badge-success">Online</span>
+          </td>
+          <td class="align-middle text-center">
+            <span class="text-secondary text-xs font-weight-bold">23/04/18</span>
+          </td>
+          <td class="align-middle">
+            <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+              Edit
+            </a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <div class="d-flex px-2 py-1">
+              <div>
+                <img src="https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/team-3.jpg" class="avatar avatar-sm me-3">
+              </div>
+              <div class="d-flex flex-column justify-content-center">
+                <h6 class="mb-0 text-xs">Alexa Liras</h6>
+                <p class="text-xs text-secondary mb-0">alexa@creative-tim.com</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p class="text-xs font-weight-bold mb-0">Programator</p>
+            <p class="text-xs text-secondary mb-0">Developer</p>
+          </td>
+          <td class="align-middle text-center text-sm">
+            <span class="badge badge-sm badge-secondary">Offline</span>
+          </td>
+          <td class="align-middle text-center">
+            <span class="text-secondary text-xs font-weight-bold">11/01/19</span>
+          </td>
+          <td class="align-middle">
+            <a href="#!" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+              Edit
+            </a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <div class="d-flex px-2 py-1">
+              <div>
+                <img src="https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/team-4.jpg" class="avatar avatar-sm me-3">
+              </div>
+              <div class="d-flex flex-column justify-content-center">
+                <h6 class="mb-0 text-xs">Laurent Perrier</h6>
+                <p class="text-xs text-secondary mb-0">laurent@creative-tim.com</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p class="text-xs font-weight-bold mb-0">Executive</p>
+            <p class="text-xs text-secondary mb-0">Projects</p>
+          </td>
+          <td class="align-middle text-center text-sm">
+            <span class="badge badge-sm badge-success">Online</span>
+          </td>
+          <td class="align-middle text-center">
+            <span class="text-secondary text-xs font-weight-bold">19/09/17</span>
+          </td>
+          <td class="align-middle">
+            <a href="#!" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+              Edit
+            </a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <div class="d-flex px-2 py-1">
+              <div>
+                <img src="https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/team-3.jpg" class="avatar avatar-sm me-3">
+              </div>
+              <div class="d-flex flex-column justify-content-center">
+                <h6 class="mb-0 text-xs">Michael Levi</h6>
+                <p class="text-xs text-secondary mb-0">michael@creative-tim.com</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p class="text-xs font-weight-bold mb-0">Programator</p>
+            <p class="text-xs text-secondary mb-0">Developer</p>
+          </td>
+          <td class="align-middle text-center text-sm">
+            <span class="badge badge-sm badge-success">Online</span>
+          </td>
+          <td class="align-middle text-center">
+            <span class="text-secondary text-xs font-weight-bold">24/12/08</span>
+          </td>
+          <td class="align-middle">
+            <a href="#!" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+              Edit
+            </a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <div class="d-flex px-2 py-1">
+              <div>
+                <img src="https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/team-2.jpg" class="avatar avatar-sm me-3">
+              </div>
+              <div class="d-flex flex-column justify-content-center">
+                <h6 class="mb-0 text-xs">Richard Gran</h6>
+                <p class="text-xs text-secondary mb-0">richard@creative-tim.com</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p class="text-xs font-weight-bold mb-0">Manager</p>
+            <p class="text-xs text-secondary mb-0">Executive</p>
+          </td>
+          <td class="align-middle text-center text-sm">
+            <span class="badge badge-sm badge-secondary">Offline</span>
+          </td>
+          <td class="align-middle text-center">
+            <span class="text-secondary text-xs font-weight-bold">04/10/21</span>
+          </td>
+          <td class="align-middle">
+            <a href="#!" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+              Edit
+            </a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <div class="d-flex px-2 py-1">
+              <div>
+                <img src="https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/team-4.jpg" class="avatar avatar-sm me-3">
+              </div>
+              <div class="d-flex flex-column justify-content-center">
+                <h6 class="mb-0 text-xs">Miriam Eric</h6>
+                <p class="text-xs text-secondary mb-0">miriam@creative-tim.com</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p class="text-xs font-weight-bold mb-0">Programtor</p>
+            <p class="text-xs text-secondary mb-0">Developer</p>
+          </td>
+          <td class="align-middle text-center text-sm">
+            <span class="badge badge-sm badge-secondary">Offline</span>
+          </td>
+          <td class="align-middle text-center">
+            <span class="text-secondary text-xs font-weight-bold">14/09/20</span>
+          </td>
+          <td class="align-middle">
+            <a href="#!" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+              Edit
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+
+  .btn btn-success,.btn btn-danger,.btn btn-warning,.btn btn-info {
+    display: inline;
+  }
+</style>
